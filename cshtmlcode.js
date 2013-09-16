88895b1f1b2a79eda6057f1706647bec661504b4
Sadegh Sadrzadeh <saadin195@gmail.com>
2013-09-16 23:59:01 +0430

    Unicode support for HTML Code feature, new license and readme
    HTML Code now supports Unicode characters.
    added MIT License.
    added README file.
    added crx file.
    version 2.0.2

--- cshtmlcode.js
+++ cshtmlcode.js
@@ -5,13 +5,16 @@ var text = "";
 for(var i = txtarea.selectionStart ; i < txtarea.selectionEnd ; i++)
 {
 	var c = (txtarea.value).charCodeAt(i);
-	if(c==32) {
+	var code = fixedCharCodeAt(txtarea.value,i);
+	if(!code)continue;
+	if(code==32) {
 		text+="&nbsp;";
 	}
-	else if(c>32){
-		text += "&#"+c+";";
+	else if(code>32){
+		text += "&#"+code+";";
 	}
-	else text+=String.fromCharCode(c);	
+	else text+=String.fromCharCode(code);	
+
 }
 
 
@@ -24,4 +27,27 @@ txtarea.value = front+text+end;
 txtarea.selectionStart = nextpos;
 txtarea.selectionEnd = nextpos;
 
-
+//function from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/charCodeAt
+function fixedCharCodeAt (str, idx) {
+    // ex. fixedCharCodeAt ('\uD800\uDC00', 0); // 65536
+    // ex. fixedCharCodeAt ('\uD800\uDC00', 1); // 65536
+    idx = idx || 0;
+    var code = str.charCodeAt(idx);
+    var hi, low;
+    if (0xD800 <= code && code <= 0xDBFF) { // High surrogate (could change last hex to 0xDB7F to treat high private surrogates as single characters)
+        hi = code;
+        low = str.charCodeAt(idx+1);
+        if (isNaN(low)) {
+            throw 'High surrogate not followed by low surrogate in fixedCharCodeAt()';
+        }
+        return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;
+    }
+    if (0xDC00 <= code && code <= 0xDFFF) { // Low surrogate
+        // We return false to allow loops to skip this iteration since should have already handled high surrogate above in the previous iteration
+        return false;
+        /*hi = str.charCodeAt(idx-1);
+        low = code;
+        return ((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000;*/
+    }
+    return code;
+} 
